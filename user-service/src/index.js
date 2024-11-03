535a7a0c53cf552cad854ebaeb8658e25680108c
AndersonRichard <Richard-r3@hotmail.com>
2024-11-03 19:40:56 -0300

    Adiciona rota para buscar usuario pelo ID

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -5,10 +5,12 @@ const port = 3000;
 
 const post_users_router = require('./routes/post_users')
 const get_users_router = require('./routes/get_users');
+const get_user_id_router = require('./routes/get_users_id')
 
 app.use(express.json());
 app.use(post_users_router)
 app.use(get_users_router);
+app.use(get_user_id_router);
 
 app.get('/status', (req, res) => {
     res.status(200);
---
+++ user-service/src/routes/get_users_id.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { User } = require('../models/index');
+const router = express.Router();
+
+
+router.get('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const user = await User.findByPk(userId); 
+
+        if (!user) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+
+        res.json(user.toJSON());
+    } catch (error) {
+        console.error('Erro ao buscar usuário:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+module.exports = router;
