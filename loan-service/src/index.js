33b23315c7d42f2506afe5c34e093b8528d1de42
jeanarthur <jean.arthursa@gmail.com>
2024-11-24 16:34:29 -0300

    adiciona rota de devolucao

--- loan-service/src/index.js
+++ loan-service/src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3002;
 
 const post_loans_router = require('./routes/post_loans');
+const post_loans_id_return_router = require('./routes/post_loans_id_return');
 const get_loans_router = require('./routes/get_loans');
 
 app.use(express.json());
@@ -14,6 +15,7 @@ app.get('/status', (req, res) => {
 });
 
 app.use(post_loans_router);
+app.use(post_loans_id_return_router);
 app.use(get_loans_router);
 
 app.listen(port, ()=>{
