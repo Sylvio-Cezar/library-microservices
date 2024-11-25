33b73483b53290c27e7a11e578b28e9250c51f1a
Alyssonjff <alyssonjfranca@gmail.com>
2024-11-24 22:05:47 -0300

    add rota/index

--- book-service/src/index.js
+++ book-service/src/index.js
@@ -7,6 +7,7 @@ const port = 3001;
 const post_books_router = require('./routes/post_books');
 const get_books_router = require('./routes/get_books');
 const get_books_id_router = require('./routes/get_books_id');
+const patch_books_id_router = require('./routes/patch_books_id');
 
 
 app.use(express.json());
@@ -16,6 +17,7 @@ app.use("/docs", swaggerUIPath.serve, swaggerUIPath.setup(swaggerjsonFilePath));
 app.use(post_books_router);
 app.use(get_books_router);
 app.use(get_books_id_router);
+app.use(patch_books_id_router);
 
 app.get('/status', (req, res) => {
     res.status(200);
