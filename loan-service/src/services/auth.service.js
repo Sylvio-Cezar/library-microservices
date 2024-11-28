896373eb03dbfca8f9c09623f0b26b6da17f8954
Sylvio Cézar <sylvio.015@gmail.com>
2024-11-28 10:58:50 -0300

    adiciona validações ao realizar um empréstimo

---
+++ loan-service/src/services/auth.service.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+async function checkLoanAvailability(bookId, userId) {
+    try {
+        const bookResponse = await axios.get(`http://localhost:3001/books/${bookId}`);
+        const book = bookResponse.data;
+
+        const userResponse = await axios.get(`http://localhost:3000/users/${userId}`);
+        const user = userResponse.data;
+
+        console.log(book);
+        console.log(book.available);
+        console.log(user);
+
+        if (user && book && book.available) {
+            await axios.patch(`http://localhost:3001/books/${bookId}`, {
+                available: false
+            });
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.error(`Erro ao checar a disponibilidade: ${err}`);
+        return false;
+    }
+}
+
+module.exports = { checkLoanAvailability };
